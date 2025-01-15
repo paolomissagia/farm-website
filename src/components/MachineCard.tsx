171a30fa6afb70b69e6c76dc58cec76b13d9b1be
Paolo Missagia <login@paolomissagia.com>
2025-01-15 17:10:29 +0000

    fixed issue on height card

--- src/components/MachineCard.tsx
+++ src/components/MachineCard.tsx
@@ -14,29 +14,31 @@ export default function MachineCard({ machine }: MachineCardProps) {
   const [isBookingModalOpen, setIsBookingModalOpen] = useState<boolean>(false);
 
   return (
-    <Card className="w-full max-w-sm">
-      <CardHeader>
-        <img
-          src={machine.image}
-          alt={machine.name}
-          className="h-48 w-full object-contain"
-        />
-      </CardHeader>
-      <CardContent>
-        <CardTitle className="text-xl">{machine.name}</CardTitle>
-        <div>
-          <p className="mb-4 text-gray-600">{machine.description}</p>
-          <div className="space-y-2">
-            <p className="text-2xl font-bold">€{machine.price}/Tag</p>
-            {machine.id === 1 && (
-              <>
-                <p className="text-lg">€{machine.hourlyPrice}/Stunde</p>
-                <p className="text-lg">€{machine.weekendPrice}/Wochenende</p>
-              </>
-            )}
+    <Card className="flex min-h-[24rem] w-full max-w-sm flex-col">
+      <div className="flex-grow">
+        <CardHeader>
+          <img
+            src={machine.image}
+            alt={machine.name}
+            className="h-48 w-full object-contain"
+          />
+        </CardHeader>
+        <CardContent>
+          <CardTitle className="text-xl">{machine.name}</CardTitle>
+          <div>
+            <p className="mb-4 text-gray-600">{machine.description}</p>
+            <div className="space-y-2">
+              <p className="text-2xl font-bold">€{machine.price}/Tag</p>
+              {machine.id === 1 && (
+                <>
+                  <p className="text-lg">€{machine.hourlyPrice}/Stunde</p>
+                  <p className="text-lg">€{machine.weekendPrice}/Wochenende</p>
+                </>
+              )}
+            </div>
           </div>
-        </div>
-      </CardContent>
+        </CardContent>
+      </div>
       <CardFooter>
         <Button className="w-full" onClick={() => setIsBookingModalOpen(true)}>
           Jetzt buchen
