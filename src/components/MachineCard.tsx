e45f211538f95ef00e118f11bc4aba3906f50091
Paolo Missagia <login@paolomissagia.com>
2025-02-20 19:41:05 +0000

    moved to object-contain

--- src/components/MachineCard.tsx
+++ src/components/MachineCard.tsx
@@ -20,7 +20,7 @@ export default function MachineCard({ machine }: MachineCardProps) {
           <img
             src={machine.image}
             alt={machine.name}
-            className="h-48 w-full object-cover"
+            className="h-48 w-full object-contain"
           />
         </CardHeader>
         <CardContent>
